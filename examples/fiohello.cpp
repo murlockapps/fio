0201d9e400437f1445187497b39bab96daeaeec2
murlockapps <79762198+murlockapps@users.noreply.github.com>
2026-09-04 16:02:37 +0200

    Update fiohello.cpp
    Update code

--- examples/fiohello.cpp
+++ examples/fiohello.cpp
@@ -1,4 +1,4 @@
-// $VER: fiohello.cpp V1.0 (27.02.2021)
+// $VER: fiohello.cpp V1.1 (04.09.2026)
 
 /* <COMPILE>
 g++ -O2 -o fiohello fiohello.cpp
@@ -11,14 +11,14 @@ g++ -O2 -o fiohello fiohello.cpp
 #include "fio.h"
 
 int main(int argc, char **argv) {
-  FILE *fp=fileOpen("/tmp/hello.txt", "wb");
+  FILE *fp = fileOpen("fiohello.txt", "wb");
   if (fp) {
     std::vector<uint8_t> v;
-    const char *cstr="Hello World!\n";
-    for (size_t i=0; cstr[i]!='\0'; i++) {
+    const char *cstr = "Hello World!\n";
+    for (size_t i = 0; cstr[i] != '\0'; i++) {
       v.push_back(cstr[i]);
     }
-    fileSaveBytes(fp, v, v.size());
+    fwrite_bytes(fp, v, v.size());
     fileClose(fp);
   }
   return 0;
